90ae93c3e90a2ad30350526de3966fb8dea34e8a
abangrephol <abang.zeze@gmail.com>
2015-05-12 07:37:45 +0700

    Changes

--- app/routes.php
+++ app/routes.php
@@ -39,52 +39,7 @@ Route::group(['domain' => '{projectSlug}.{tld}'], function()
         }
     });
 
-    Route::group(array('namespace'=>'Admin', 'prefix'=>'admin'),function(){
-        Route::get('/login',array('as'=>'admin/login','uses'=>'SiteController@login'));
-        Route::get('/logout',array('as'=>'admin/logout','uses'=>'SiteController@logout'));
-        Route::post('/login','SiteController@login');
-        Route::group(array('before'=>'admin_auth'),function(){
-            Route::get('/',function(){
-                return Redirect::to('admin/dashboard');
-            });
-            Route::get('dashboard',array('as'=>'admin/dashboard','uses'=>'SiteController@index'));
-            Route::resource('companies','CompaniesController',array('names' => array('index'=>'admin/companies')));
-            Route::resource('categories','CategoriesController',array('names' => array('index'=>'admin/categories')));
 
-            Route::resource('custom-website','CustomWebsitesController',array('names' => array('index'=>'admin/custom-website')));
-            Route::get('custom-website/{id}/choose-templates',array('as'=>'custom-website.chooseTemplate','uses'=>'CustomWebsitesController@chooseTemplates'));
-            Route::get('custom-website/{id}/builder/{templateId}',array('as'=>'custom-website.builder','uses'=>'CustomWebsitesController@builder'));
-            Route::get('custom-website/{id}/builder-editor/{templateId}',array('as'=>'custom-website.builderEditor','uses'=>'CustomWebsitesController@builderEditor'));
-            Route::post('custom-website/{id}/builder-save',array('as'=>'custom-website.builderSave','uses'=>'CustomWebsitesController@builderSave'));
-            Route::get('custom-website/{id}/pages',array('as'=>'custom-website.pages','uses'=>'CustomWebsitesController@pages'));
-            Route::get('custom-website/{id}/pages-delete/{pageId}',array('as'=>'custom-website.pages.delete','uses'=>'CustomWebsitesController@pagesDelete'));
-            Route::resource('custom-template','CustomTemplatesController',array('names' => array('index'=>'admin/custom-template')));
-
-            Route::group(array('prefix'=>'dt'),function(){
-                Route::get('company',array('as'=>'dt.company','uses'=>'CompaniesController@getDatatableAll'));
-                Route::get('category',array('as'=>'dt.category','uses'=>'CategoriesController@getDatatableAll'));
-                Route::get('custom-website',array('as'=>'dt.custom-website','uses'=>'CustomWebsitesController@getDatatableAll'));
-                Route::get('custom-website-pages',array('as'=>'dt.custom-website-pages','uses'=>'CustomWebsitesController@getWebsitePages'));
-            });
-
-
-        });
-
-    });
-
-
-    //Route::get('/website/{id}','WebsiteController@website');
-
-    //Route::get('/','HomeController@index');
-    Route::get('/contact-us','HomeController@contact');
-    Route::get('/about-us','HomeController@about');
-    Route::get('/price-listing','HomeController@pricelisting');
-    Route::group(array('prefix'=>'companies'),function(){
-        //Route::get('/','CategoriesController@index');
-        Route::get('/','CategoriesController@index');
-        Route::get('{categorySlug}','CategoriesController@slug');
-        Route::get('detail/{id}','CompaniesController@index');
-    });
 
     Route::get('/page/{slug}', function($projectSlug,$slug) {
         $app = app();
@@ -98,6 +53,51 @@ Route::group(['domain' => '{projectSlug}.{tld}'], function()
         }
     });
 });
+Route::group(array('namespace'=>'Admin', 'prefix'=>'admin'),function(){
+    Route::get('/login',array('as'=>'admin/login','uses'=>'SiteController@login'));
+    Route::get('/logout',array('as'=>'admin/logout','uses'=>'SiteController@logout'));
+    Route::post('/login','SiteController@login');
+    Route::group(array('before'=>'admin_auth'),function(){
+        Route::get('/',function(){
+            return Redirect::to('admin/dashboard');
+        });
+        Route::get('dashboard',array('as'=>'admin/dashboard','uses'=>'SiteController@index'));
+        Route::resource('companies','CompaniesController',array('names' => array('index'=>'admin/companies')));
+        Route::resource('categories','CategoriesController',array('names' => array('index'=>'admin/categories')));
 
+        Route::resource('custom-website','CustomWebsitesController',array('names' => array('index'=>'admin/custom-website')));
+        Route::get('custom-website/{id}/choose-templates',array('as'=>'custom-website.chooseTemplate','uses'=>'CustomWebsitesController@chooseTemplates'));
+        Route::get('custom-website/{id}/builder/{templateId}',array('as'=>'custom-website.builder','uses'=>'CustomWebsitesController@builder'));
+        Route::get('custom-website/{id}/builder-editor/{templateId}',array('as'=>'custom-website.builderEditor','uses'=>'CustomWebsitesController@builderEditor'));
+        Route::post('custom-website/{id}/builder-save',array('as'=>'custom-website.builderSave','uses'=>'CustomWebsitesController@builderSave'));
+        Route::get('custom-website/{id}/pages',array('as'=>'custom-website.pages','uses'=>'CustomWebsitesController@pages'));
+        Route::get('custom-website/{id}/pages-delete/{pageId}',array('as'=>'custom-website.pages.delete','uses'=>'CustomWebsitesController@pagesDelete'));
+        Route::resource('custom-template','CustomTemplatesController',array('names' => array('index'=>'admin/custom-template')));
+
+        Route::group(array('prefix'=>'dt'),function(){
+            Route::get('company',array('as'=>'dt.company','uses'=>'CompaniesController@getDatatableAll'));
+            Route::get('category',array('as'=>'dt.category','uses'=>'CategoriesController@getDatatableAll'));
+            Route::get('custom-website',array('as'=>'dt.custom-website','uses'=>'CustomWebsitesController@getDatatableAll'));
+            Route::get('custom-website-pages',array('as'=>'dt.custom-website-pages','uses'=>'CustomWebsitesController@getWebsitePages'));
+        });
+
+
+    });
+
+});
+
+
+//Route::get('/website/{id}','WebsiteController@website');
+
+//Route::get('/','HomeController@index');
+Route::get('/contact-us','HomeController@contact');
+Route::get('/about-us','HomeController@about');
+Route::get('/price-listing','HomeController@pricelisting');
+Route::group(array('prefix'=>'companies'),function(){
+    //Route::get('/','CategoriesController@index');
+    Route::get('/','CategoriesController@index');
+    Route::get('{categorySlug}','CategoriesController@slug');
+    Route::get('detail/{id}','CompaniesController@index');
+});
 
 
