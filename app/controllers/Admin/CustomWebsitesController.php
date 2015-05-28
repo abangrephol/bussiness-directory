ee4c272a92b9dce4779a4f0b500ed7825596f866
abangrephol <abang.zeze@gmail.com>
2015-05-28 13:04:05 +0700

    Test change

--- app/controllers/Admin/CustomWebsitesController.php
+++ app/controllers/Admin/CustomWebsitesController.php
@@ -227,7 +227,7 @@ class CustomWebsitesController extends BaseController {
         session_start();
         $_SESSION["RF"]["subfolder"] = "$id";
         echo $path = public_path().'/uploads/images/' . $id;
-        \File::makeDirectory($path, $mode = 0777, true, true);
+        \File::makeDirectory($path, $mode = 0777, true);
         if(isset($pageId) && $pageId!=0){
             $data['data'] = \CustomWebsitePage::find($pageId);
             $custom_data = json_decode(\CustomWebsitePage::find($pageId)->custom_data);
